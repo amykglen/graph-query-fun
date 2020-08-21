26f115e61704c99d99579e66f32a4f6ac6393e71
Amy Glen <amykglen@gmail.com>
2020-08-20 17:22:32 -0700

    Make multihop queries possible (but allows backtracking)

--- graph-querier-scala/src/main/scala/Main.scala
+++ graph-querier-scala/src/main/scala/Main.scala
@@ -11,19 +11,46 @@ object GraphQuerier extends App {
 
   // Search for nodes connected to specified node (input via sbt shell)
   val inputNodeID: String = if (args.length >= 1) args(0) else "CUI:111"
-  val outputNodeType: String = if (args.length > 1) args(1) else ""
-  val edgeType: String = if (args.length > 2) args(2) else ""
-  val answerNodeIDs: Set[String] = findConnectedNodes(inputNodeID, outputNodeType, edgeType, edgeRows, nodeIDsByType)
+  val numHops: Int = if (args.length > 1) args(1).toInt else 1
+  val outputNodeType: String = if (args.length > 2) args(2) else ""
+  val answerNodeIDs: Set[String] = runQuery(inputNodeID, outputNodeType, numHops, edgeRows, nodeIDsByType)
+  reportFindings(answerNodeIDs)
 
-  // Report our findings
-  if (answerNodeIDs.isEmpty) {
-    println("No answers found.")
-  } else {
-    val numAnswers: Int = answerNodeIDs.size
-    println(s"Found $numAnswers answers:")
-    answerNodeIDs.foreach(nodeID => println(nodeID))
+
+  def runQuery(inputNodeID: String, outputNodeType: String = "", numHops: Int, edgeRows: Array[Array[String]], nodeIDsByType: Map[String, Set[String]]): Set[String] = {
+    val allConnectedNodeIDs: Set[String] = findConnectedNodes(inputNodeID, numHops, edgeRows).diff(Set(inputNodeID))
+    val nodeIDsOfProperType: Set[String] = if (outputNodeType == "") allConnectedNodeIDs else allConnectedNodeIDs.intersect(nodeIDsByType.getOrElse(outputNodeType, Set()))
+    nodeIDsOfProperType
   }
 
+  def findConnectedNodes(inputNodeID: String, numHops: Int, edgeRows: Array[Array[String]]): Set[String] = {
+    if (numHops <= 0) {
+      Set()
+    } else {
+      val directNeighbors: Set[String] = findDirectNeighbors(inputNodeID, edgeRows)
+      if (numHops == 1) {
+        directNeighbors
+      } else {
+        directNeighbors.flatMap(nodeID => findConnectedNodes(nodeID, numHops - 1, edgeRows))
+      }
+    }
+  }
+
+  def findDirectNeighbors(inputNodeID: String, edgeRows: Array[Array[String]]): Set[String] = {
+    val edgesUsingNode = edgeRows.filter(row => row(1) == inputNodeID || row(2) == inputNodeID)
+    val connectedNodeIDs = edgesUsingNode.map(row => row.slice(1, 3)).flatten.toSet.diff(Set(inputNodeID)) // Why can't I get flatMap to work here?
+    connectedNodeIDs
+  }
+
+  def reportFindings(answerNodeIDs: Set[String]): Unit = {
+    if (answerNodeIDs.isEmpty) {
+      println("No answers found.")
+    } else {
+      val numAnswers: Int = answerNodeIDs.size
+      println(s"Found $numAnswers answers:")
+      answerNodeIDs.foreach(nodeID => println(nodeID))
+    }
+  }
 
   def extractDataFromTSV(fileName: String, numColumns: Int): Array[Array[String]] = {
     // Thank you https://alvinalexander.com/scala/csv-file-how-to-process-open-read-parse-in-scala/
@@ -35,19 +62,6 @@ object GraphQuerier extends App {
       rows(count) = line.split("\t").map(_.trim)
     }
     source.close
-    return rows
-  }
-
-  def findConnectedNodes(inputNodeID: String, outputNodeType: String, edgeType: String = "", edgeRows: Array[Array[String]], nodeIDsByType: Map[String, Set[String]]): Set[String] = {
-    // TODO: make search more efficient by using more complex data structures
-    val edgesUsingNode = edgeRows.filter(row => row(1) == inputNodeID || row(2) == inputNodeID)
-    val relevantEdges = if (edgeType == "") edgesUsingNode else edgesUsingNode.filter(row => row(3) == edgeType)
-    val connectedNodeIDs = getNodeIDsUsedByEdges(relevantEdges).diff(Set(inputNodeID))
-    val relevantNodeIDs = if (outputNodeType == "") connectedNodeIDs else connectedNodeIDs.intersect(nodeIDsByType.getOrElse(outputNodeType, Set()))
-    return relevantNodeIDs
-  }
-
-  def getNodeIDsUsedByEdges(edgeRows: Array[Array[String]]): Set[String] = {
-    return edgeRows.map(row => row.slice(1, 3)).flatten.toSet  // Why can't I get flatMap to work here?
+    rows
   }
 }
